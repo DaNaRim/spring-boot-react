5d18dddcecc0472854c8d4a03cd76048bcc60bbe
DaNaRim <rangar.danarim@gmail.com>
2022-09-19 17:49:26 +0300

    INIT

---
+++ src/main/java/com/danarim/springBootReact/SpringBootReactApplication.java
@@ -0,0 +1,13 @@
+package com.danarim.springBootReact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootReactApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/danarim/springBootReact/SpringBootReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danarim.springBootReact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
